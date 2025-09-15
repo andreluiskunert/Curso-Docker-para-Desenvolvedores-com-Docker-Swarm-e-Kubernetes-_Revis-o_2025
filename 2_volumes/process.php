1127451578a9a27ab5822bc4fd288323a7943401
Andre Luis Kunert <seuemail@example.com>
2025-09-15 20:46:30 -0300

    Bora Codar Docker/Swarm/Kubernetes_2025

--- 2_volumes/process.php
+++ 2_volumes/process.php
@@ -1,11 +1,17 @@
 <?php
-    $message = $_POST["message"];
-    $files = scandir("./messages");
-    $num_files = count($files) - 2; // . e ..
-    $fileName = "msg-{$num_files}.txt";
-    $file = fopen("./messages/{$fileName}", "X");
-    fwrite($file, $message);
-    fclose($file);
-    header("Localtion: index.php");
 
-?>
+  $message = $_POST["message"];
+
+  $files = scandir("./messages");
+  $num_files = count($files) - 2; // . e ..
+
+$fileName = "msg-{$num_files}.txt";
+
+$file = fopen("./messages/{$fileName}", "w");
+
+
+fwrite($file, $message);
+
+fclose($file);
+
+  header("Location: index.php");
